92b92260cfa23470d5502a17f6b138c18d22df8e
smokin joe <joe.ekiert@gmail.com>
2014-11-01 08:27:02 +0000

    initial commit

---
+++ script/main.js
@@ -0,0 +1,42 @@
+var app = playground({
+
+  create: function() {
+    
+  },
+
+  ready: function() {
+
+  },
+
+  step: function(delta) {
+
+  },
+
+  render: function(delta) {
+
+    this.layer.clear("#008");
+    this.layer.fillStyle("#fff").font("64px Arial").fillText("Hello World!", 32, 64);
+
+  },
+
+  mousedown: function(event) {
+
+  },
+
+  mouseup: function(event) {
+
+  },
+
+  mousemove: function(event) {
+
+  },
+
+  keydown: function(event) {
+
+  },
+
+  keyup: function(event) {
+
+  }
+
+});
